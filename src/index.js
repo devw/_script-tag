126f10c0800445460f7be0d73e5ea545439d1f5a
Antonio Pierro <antonio.pierro@gmail.com>
2020-09-24 22:34:49 +0200

    moved modal into index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-import { Api } from "./graphql";
+import { query } from "./query";
+import "./style.css";
 
-function component() {
+const component = () => {
+    window["MyScript"] = { query: query };
     const element = document.createElement("div");
+    element.innerHTML = `
+        <div class="bg-modal">
+            <div class="content-modal">
+                <div class="close">+</div>
+            </div>
+        </div>
+    `;
 
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = "Hello webpack";
-    window["MyScript"] = Api;
     return element;
-}
+};
+
+const handleAccountClick = (e) => {
+    e.preventDefault();
+    document.querySelector(".bg-modal").style.display = "flex";
+};
 
 document.body.appendChild(component());
+
+document
+    .querySelector(".site-header__account")
+    .addEventListener("click", handleAccountClick);
+
+document.querySelector(".close").addEventListener("click", () => {
+    document.querySelector(".bg-modal").style.display = "none";
+});
