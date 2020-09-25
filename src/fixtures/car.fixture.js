4678bb194cb212765e0ee015936b5c7d9134e95a
Antonio Pierro <antonio.pierro@gmail.com>
2020-09-25 14:27:10 +0200

    adding fixtures

---
+++ src/fixtures/car.fixture.js
@@ -0,0 +1,61 @@
+export const carFixture = {
+    "data": {
+      "product": {
+        "title": "car",
+        "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "onlineStoreUrl": null,
+        "variants": {
+          "edges": [
+            {
+              "node": {
+                "id": "gid://shopify/ProductVariant/36296387068067",
+                "displayName": "car - red / 2019 / Hybrid"
+              }
+            },
+            {
+              "node": {
+                "id": "gid://shopify/ProductVariant/36296387166371",
+                "displayName": "car - red / 2020 / Hybrid"
+              }
+            },
+            {
+              "node": {
+                "id": "gid://shopify/ProductVariant/36296387231907",
+                "displayName": "car - blue / 2019 / Hybrid"
+              }
+            },
+            {
+              "node": {
+                "id": "gid://shopify/ProductVariant/36296387362979",
+                "displayName": "car - blue / 2020 / Hybrid"
+              }
+            }
+          ]
+        },
+        "options": [
+          {
+            "id": "gid://shopify/ProductOption/7263238652067",
+            "name": "color"
+          },
+          {
+            "id": "gid://shopify/ProductOption/7294481760419",
+            "name": "year"
+          },
+          {
+            "id": "gid://shopify/ProductOption/7299607756963",
+            "name": "type"
+          }
+        ]
+      }
+    },
+    "extensions": {
+      "cost": {
+        "requestedQueryCost": 14,
+        "actualQueryCost": 8,
+        "throttleStatus": {
+          "maximumAvailable": 1000,
+          "currentlyAvailable": 992,
+          "restoreRate": 50
+        }
+      }
+    };
