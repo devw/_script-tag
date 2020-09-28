048f50bc37c6b86079443073cf43ee01f7ce6701
Antonio Pierro <antonio.pierro@gmail.com>
2020-09-28 18:31:02 +0200

    adding sass-loader and sass

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import { graphql } from "./services/graphql";
 import { modalComponent } from "./components/modal/modal";
+import { account } from "./components/account-popup/account";
 import { productsQuery } from "./queries/products.query";
 import { modalHook } from "./hooks/modal.hook";
 
 globalThis["MyScript"] = { graphql: graphql(productsQuery) };
 
 document.body.appendChild(modalComponent());
+document.body.appendChild(account());
 modalHook();
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,17 @@ module.exports = {
                 test: /\.html$/i,
                 loader: "html-loader",
             },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
         ],
     },
 };
