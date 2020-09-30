cc6c6a70443175aee7048381d85d3c13faca869a
Antonio Pierro <antonio.pierro@gmail.com>
2020-09-30 12:10:11 +0200

    adding register component

---
+++ src/components/register/register.js
@@ -0,0 +1,8 @@
+import "./register.scss";
+import html from "./register.html";
+
+export const registerComponent = () => {
+    const element = document.createElement("div");
+    element.innerHTML = html;
+    return element;
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import { graphql } from "./services/graphql";
 import { modalComponent } from "./components/modal/modal";
-import { emailComponent } from "./components/email/email";
 import { productsQuery } from "./queries/products.query";
 import { modalHook } from "./hooks/modal.hook";
+import { emailComponent } from "./components/email/email";
+import { registerComponent } from "./components/register/register";
 
 globalThis["MyScript"] = { graphql: graphql(productsQuery) };
 
 document.body.appendChild(modalComponent());
 
-document.querySelector(".main-modal").appendChild(emailComponent());
+document.querySelector(".main-modal").appendChild(registerComponent());
 
 modalHook();
