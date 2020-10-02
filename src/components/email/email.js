6687a3f769b3ebb747ac14a5b11cb6b98c903b6c
Antonio Pierro <antonio.pierro@gmail.com>
2020-10-02 15:06:59 +0200

    refactoring emailComponent

--- src/components/email/email.js
+++ src/components/email/email.js
@@ -1,17 +1,15 @@
 import "./email.scss";
 import html from "./email.html";
 
-const init = (node) => {
-    const updateValue = (e) =>
-        verifyEmail(e.target.value)
-            ? node.querySelector("button").removeAttribute("disabled")
-            : node.querySelector("button").setAttribute("disabled", "true");
-    const verifyEmail = (e) => /\S+@\S+\.\S+/.test(e);
-    node.querySelector(".input-text").addEventListener("input", updateValue);
-};
+const toggleButton = (e) =>
+    /\S+@\S+\.\S+/.test(e.target.value)
+        ? node.querySelector("button").removeAttribute("disabled")
+        : node.querySelector("button").setAttribute("disabled", "true");
 
-export const emailComponent = () => {
-    const element = document.createElement("div");
-    element.innerHTML = html;
-    return { dom: element, callback: init };
-};
+const init = (node) =>
+    node.querySelector(".input-text").addEventListener("input", toggleButton);
+
+const node = document.createElement("div");
+node.innerHTML = html;
+
+export const emailComponent = () => ({ dom: node, callback: init });
