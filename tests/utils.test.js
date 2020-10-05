2fef1e5b655ec3cc2809cddaad6548f1fe02de3f
Antonio Pierro <antonio.pierro@gmail.com>
2020-10-05 23:48:05 +0200

    adding serialize test

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,5 +1,12 @@
-// import serialize from "../src/utils"; TODO to fix
+import { serialize } from "../src/utils";
 
 test("It should retrieving a FormData object from an HTML form", () => {
-    expect(1 + 2).toBe(3);
+    const formElement = document.createElement("form");
+    expect(serialize(formElement)).toMatchObject({});
+});
+
+test("It should retrieving a FormData object from an HTML form", () => {
+    const formElement = document.createElement("form");
+    formElement.innerHTML = "<input name='age' value='10'>";
+    expect(serialize(formElement)).toMatchObject({ age: "10" });
 });
